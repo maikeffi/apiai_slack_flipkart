0934cb55bebb48e4d17796e76c4fadb6a03a9529
Kannan Maikeffi <maikeffi@gmail.com>
2017-05-24 00:13:41 +0530

    Completed adding webhook interface .. for dummy interactive messages for slack

--- app.py
+++ app.py
@@ -1,29 +1,104 @@
-import requests
+import os
 import json
+from flask import Flask
+from flask import request
+from flask import make_response
 
-fkAffiliateId = "******"
-fkAffiliateToken = "*****"
-
-#url = "https://affiliate-api.flipkart.net/affiliate/search/json"
-url = "https://affiliate-api.flipkart.net/affiliate/api/" + fkAffiliateId + ".json";
-#url = "https://affiliate-api.flipkart.net/affiliate/1.0/feeds/uve2payle/category/ckf-czl.json?expiresAt=1495512790980&sig=7dd254b047f571a027b2b6884ad0eb39"
-#urlv1 = "https://affiliate-api.flipkart.net/affiliate/1.0/search.json?query={productString}&resultCount={count}"
-#product = "Sony GTK-N1BT Mini Hi-Fi System(Black)"
-#count = "1"
-
-header = {"Fk-Affiliate-Id":fkAffiliateId,"Fk-Affiliate-Token":fkAffiliateToken}
-
-#url = urlv1.replace("{productString}",product).replace("{count}",count)
+app = Flask(__name__)
 
 
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
 
-r = requests.get(url,headers=header)
-data = json.loads(r.text)
+    print("Request:")
+    print(json.dumps(req, indent=4))
 
-#print(r.json())
-listing = data['apiGroups']['affiliate']['apiListings']
-#for key in listing.keys():
-#    print(key)
+    res = processRequest(req)
 
-for list in listing.values():
-    print("Catagory :"+list['availableVariants']['v1.1.0']['resourceName'] +" Url : "+list['availableVariants']['v1.1.0']['get'])
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def processRequest(req):
+    if req.get("result").get("action") != "getFlipkartCat":
+        return {}
+
+    res = makeWebhookResult()
+    return res
+
+def makeWebhookResult():
+
+    print("Response:")
+
+    speech = "Flipkart Catagories"
+    slack_message = {
+        "text": "Which Catagory are you looking for?",
+        "response_type": "in_channel",
+        "attachments": [
+            {
+                "text": "Choose a Catagory",
+                "fallback": "If you could read this message, you'd be choosing something fun to do right now.",
+                "color": "#3AA3E3",
+                "attachment_type": "default",
+                "callback_id": "catagory_selection",
+                "actions": [
+                    {
+                        "name": "catagory_list",
+                        "text": "Pick a Catagory...",
+                        "type": "select",
+                        "options": [
+                            {
+                                "text": "Laptops",
+                                "value": "laptops"
+                            },
+                            {
+                                "text": "Tablets",
+                                "value": "tablets"
+                            },
+                            {
+                                "text": "Desktops",
+                                "value": "desktops"
+                            },
+                            {
+                                "text": "Computer Storage",
+                                "value": "computer_storage"
+                            },
+                            {
+                                "text": "Computer Components",
+                                "value": "computer_components"
+                            },
+                            {
+                                "text": "Laptop Accessories",
+                                "value": "laptop_accessories"
+                            },
+                            {
+                                "text": "Computer Peripherals",
+                                "value": "computer_peripherals"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+
+    print(json.dumps(slack_message))
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        "data": {"slack": slack_message},
+        # "contextOut": [],
+        "source": "apiai-weather-webhook-sample"
+    }
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print( "Starting app on port %d" % port)
+
+    app.run(debug=False, port=port, host='0.0.0.0')
+
